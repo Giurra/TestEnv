9d2043d7b34d72db305ec52cd3966a9f98f2233b
Giurra <urosgavric@gmail.com>
2016-03-13 12:34:52 +0100

    Initial

---
+++ src/test/java/TestEnv.java
@@ -0,0 +1,32 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by urke on 13-Mar-16.
+ */
+public class TestEnv {
+    public WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testEnv() {
+        driver.get("http://giurra.org/roadtoqa/");
+        Assert.assertEquals("RoadToQA – Just another WordPress site", driver.getTitle());
+        System.out.println(driver.getTitle());
+    }
+}
